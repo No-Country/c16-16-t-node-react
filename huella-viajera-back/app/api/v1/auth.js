9b725d07923d82bfca88d0d4609b8927ba1d5718
Welinton Suarez <wesgdeveloper@gmail.com>
2024-02-18 09:25:24 -0500

    add controllers for signin and signup

---
+++ huella-viajera-back/app/api/v1/auth.js
@@ -0,0 +1,31 @@
+import jwt from "jsonwebtoken";
+
+import { configuration } from "../../config.js";
+
+const { token } = configuration;
+const { secret, expires } = token;
+
+export const signToken = (payload, expiresIn = expires) => {
+  return jwt.sign(payload, secret, {
+    expiresIn,
+  });
+};
+
+export const auth = async (req, res, next) => {
+  let token = req.headers.authorization || "";
+
+  if (token.startWith("Bearer")) {
+    token = token.slice(7);
+  }
+  if (!token) return next({ message: "Forbidden", status: 403 });
+
+  jwt.verify(token, secret, function (err, decoded) {
+    if (err) {
+      next({ message: "Forbidden", status: 403 });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
+export const owner = async (req, res, next) => {};
