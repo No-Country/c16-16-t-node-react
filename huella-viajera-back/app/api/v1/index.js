07a9b15e7a5866dbb225d2eb014f4dd0d51d3405
Welinton Suarez <wesgdeveloper@gmail.com>
2024-02-19 10:06:54 -0500

    add routes structure for owners and carers

--- huella-viajera-back/app/api/v1/index.js
+++ huella-viajera-back/app/api/v1/index.js
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import { router as users } from "./users/routes.js";
 import { router as pets } from "./pets/routes.js";
+import { router as carers } from "./carers/routes.js";
+import { router as owners } from "./owners/routes.js";
 
 // eslint-disable-next-line new-cap
 export const router = Router();
 
 router.use("/users", users);
 router.use("/pets/", pets);
+router.use("carers", carers);
+router.use("owners", owners);
---
+++ huella-viajera-back/app/api/v1/owners/controller.js
@@ -0,0 +1,4 @@
+export const id = async (req, res, next) => {};
+export const read = async (req, res, next) => {};
+export const update = async (req, res, next) => {};
+export const remove = async (req, res, next) => {};
